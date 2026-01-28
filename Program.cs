6890fd66261dfc0edad3ef020ad4a1d40aba6b52
hugo brisebois <134088623+HugoBrisebois@users.noreply.github.com>
2026-01-28 12:45:20 -0500

    started on building the conpt definiation

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello, World!\n");
+
+        Concept();
+    }
+
+
+
+    public static void Concept()
+    {
+        // Dictionary to hold all the information
+        Dictionary<int, string> Topics= new Dictionary<int, string>();
+
+        // adding topics
+        Topics.Add(1, "Math");
+        Topics.Add(2, "English");
+        Topics.Add(3, "French");
+        Topics.Add(4, "Science");
+        Topics.Add(5, "History");
+
+        // Display the dictionary contents
+
+        foreach (var item in Topics)
+        {
+            Console.WriteLine($"{item}\n");
+        }
+    }
+
+    public static void ConceptEngine()
+    {
+
+    }
+    
+}  
