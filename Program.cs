c89abafa18f155533ac078b91cfbce5c9b21d8dc
hugo brisebois <134088623+HugoBrisebois@users.noreply.github.com>
2026-01-28 18:17:15 -0500

    chore: add SQLite database file and update project references

--- Program.cs
+++ Program.cs
@@ -6,39 +6,21 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello, World!\n");
-        string userName = Console.ReadLine() ?? string.Empty;
+       // connect to the database
+       
 
-        Concept();
+        
     }
 
 
 
     public static void Concept()
     {
-        // Dictionary to hold all the information
-        Dictionary<int, string> Topics = new Dictionary<int, string>();
+        // Defining what a concept is
+        // also using a SQlite database to store all the concepts/topics as nodes like in a graph database
 
-        Topics.Add(1, "Math");
-        Topics.Add(2, "Geography");
-        Topics.Add(3, "science");
-        // Display the Topics Dictionary contents
 
-        foreach (var item in Topics)
-        {
-            Console.WriteLine($"{item}\n");
-        }
 
-        Dictionary<int, string> Concepts = new Dictionary<int, string>();
-
-        Concepts.Add(1, "Percentage");
-        Concepts.Add(2, "Matter");
-        foreach (var item1 in Concepts)
-        {
-            Console.WriteLine($"{item1}\n");
-        }
-
-        Dictionary<int, string> Links = new Dictionary<int, string>();
     }
 
     public static void ConceptEngine()
@@ -46,4 +28,4 @@ public class Program
         
     }
     
-}  
+}
