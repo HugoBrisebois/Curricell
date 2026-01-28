1f23707460aa7140efb9cf120a469962e0fdc592
hugo brisebois <134088623+HugoBrisebois@users.noreply.github.com>
2026-01-28 12:49:07 -0500

    refactor: clean up Concept method by removing redundant topic additions

--- Program.cs
+++ Program.cs
@@ -15,14 +15,9 @@ public class Program
     public static void Concept()
     {
         // Dictionary to hold all the information
-        Dictionary<int, string> Topics= new Dictionary<int, string>();
+        Dictionary<int, string> Topics = new Dictionary<int, string>();
 
-        // adding topics
-        Topics.Add(1, "Math");
-        Topics.Add(2, "English");
-        Topics.Add(3, "French");
-        Topics.Add(4, "Science");
-        Topics.Add(5, "History");
+       
 
         // Display the dictionary contents
 
