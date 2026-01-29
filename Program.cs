b2b926769f5df51e49bbdd2a69f5a2882314e785
hugo brisebois <134088623+HugoBrisebois@users.noreply.github.com>
2026-01-28 20:05:34 -0500

    figuring out how to implement my database

--- Program.cs
+++ Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.VisualBasic;
+using System.Data.SQLite;
+using System.Data.SqlTypes;
+using System.Data;
+
+
 
 public class Program
 {
     public static void Main()
     {
        // connect to the database
-       
+        Console.WriteLine("Connecting to database");
 
+        string connectionString = "Data Source=Curricel.db;version=3;";
+        using var connection= new SQLiteConnection(connectionString);
+        connection.Open();
         
+        Console.WriteLine("Sqlite Connected");
+        String SQLQueryCreateTable = @"CREATE TABLE IF NOT EXISTS Concepts(Id INTEGER PRIMARY KEY,
+        Name TEXT NOT NULL,
+        Topic STRING,
+        Description TEXT
+        )";
+
+        using var commend= new SQLiteCommand(SQLQueryCreateTable, connection);
+        commend.ExecuteNonQuery();
     }
 
 
@@ -18,8 +35,6 @@ public class Program
     {
         // Defining what a concept is
         // also using a SQlite database to store all the concepts/topics as nodes like in a graph database
-
-
 
     }
 
