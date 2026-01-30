f1d5743a9723e50b2031e2daf92881c80406dfa6
hugo brisebois <134088623+HugoBrisebois@users.noreply.github.com>
2026-01-29 19:58:34 -0500

    refactor: enhance database connection handling and table creation logic

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using Microsoft.VisualBasic;
 using System.Data.SQLite;
 using System.Data.SqlTypes;
 using System.Data;
+using System.Threading.Channels;
 
 
 
@@ -13,20 +14,40 @@ public class Program
     {
        // connect to the database
         Console.WriteLine("Connecting to database");
-
-        string connectionString = "Data Source=Curricel.db;version=3;";
-        using var connection= new SQLiteConnection(connectionString);
-        connection.Open();
-        
-        Console.WriteLine("Sqlite Connected");
-        String SQLQueryCreateTable = @"CREATE TABLE IF NOT EXISTS Concepts(Id INTEGER PRIMARY KEY,
+        string connectionString = "Data Source=Curricel.db;version=3;FailIfMissing = True";
+      
+       // defining the tables
+       String SQLQueryCreateTable = @"CREATE TABLE IF NOT EXISTS Concepts(Id    INTEGER PRIMARY KEY, 
+        Concept VARCHAR,
         Name TEXT NOT NULL,
-        Topic STRING,
         Description TEXT
         )";
+      
+        try
+        {
+            // Create a SQliteconnection object called connection
+            using (SQLiteConnection MyConnection = new SQLiteConnection(connectionString))
+            {
+                MyConnection.Open(); // open a connection to database  
 
-        using var commend= new SQLiteCommand(SQLQueryCreateTable, connection);
-        commend.ExecuteNonQuery();
+                // Drop the old Customers table if it exists
+                using (SQLiteCommand dropCommand = new SQLiteCommand("DROP TABLE IF EXISTS Customers;", MyConnection))
+                {
+                    dropCommand.ExecuteNonQuery();
+                }
+
+                using (SQLiteCommand Mycommand = new SQLiteCommand(SQLQueryCreateTable, MyConnection))
+                {
+                    var RowsChanged = Mycommand.ExecuteNonQuery(); // execute create the table sql query
+                    Console.WriteLine($"No o Rows Changes = {RowsChanged}"); // rows changed equals 0, since we are only creating a table
+                } 
+            } 
+        }
+        catch(Exception Ex)
+        {
+            Console.WriteLine(Ex.Message);
+        }
+        Console.WriteLine("Sqlite Connected");
     }
 
 
